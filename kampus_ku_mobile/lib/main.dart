61d8e9dcfa22de132678e900770d2ecc35a62eb0
raihanaiooo <raihanaaaz2@gmail.com>
2026-04-30 07:02:20 +0700

    UPDATE: tambah view buat request dosen, controller, dan juga service

--- kampus_ku_mobile/lib/main.dart
+++ kampus_ku_mobile/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:kampus_ku_mobile/core/network/mongo_database.dart';
+import 'package:kampus_ku_mobile/data/models/task_model.dart';
+import '../../../data/models/user_model.dart';
 
 // Import Auth & Pages
 import 'package:kampus_ku_mobile/features/auth/login_page.dart';
@@ -25,7 +28,6 @@ void main() async {
 
   await dotenv.load(fileName: ".env");
   print("MONGO_URL: ${dotenv.env['MONGO_URL']}");
-  await MongoDatabase.connect();
   await Hive.initFlutter();
 
   Hive.registerAdapter(ScheduleLocalModelAdapter());
@@ -34,22 +36,19 @@ void main() async {
   await Hive.openBox<ScheduleLocalModel>('schedules');
   await Hive.openBox<AnnouncementModel>('announcements');
   await Hive.openBox<TaskModel>('tasks');
+  await MongoDatabase.connect();
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          
           create: (_) => ScheduleController(ScheduleService()),
-        
         ),
         ChangeNotifierProvider(
           create: (_) => AnnouncementController(AnnouncementService()),
         ),
         ChangeNotifierProvider(
-          
           create: (_) => ScheduleRequestController(ScheduleRequestService()),
-        ,
         ),
       ],
       child: const MyApp(),
@@ -65,40 +64,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'SIGMA',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(), // ⬅️ tetap mulai dari login
-    );
-  }
-}
-
-// =========================
-//  HOME PAGE (TEST API)
-// =========================
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final scheduleService = ScheduleService();
-
-  @override
-  void initState() {
-    super.initState();
-    fetchSchedules();
-  }
-
-  void fetchSchedules() async {
-    final data = await scheduleService.getSchedules();
-    print("SCHEDULE DATA: $data");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: const Center(child: Text("Login berhasil 🎉")),
+      home: LoginPage(),
     );
   }
 }
